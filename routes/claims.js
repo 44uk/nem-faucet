b65052ba0ccfd71943031c56e4f43a6896b5ea22
Yoshiyuki Ieyama <yukku0423@gmail.com>
2018-07-28 08:47:56 +0900

    update

--- routes/claims.js
+++ routes/claims.js
@@ -71,7 +71,7 @@ router.post('/', async function(req, res, next) {
     });
 
     if(distAccount['account']['balance'] >= ENOUGH_BALANCE) {
-      throw new Error(`Your account seems to have enougth balance => (${distAccount['account']['balance'].toLocaleString()})`);
+      throw new Error(`Your account already have enougth balance => (${distAccount['account']['balance'].toLocaleString()})`);
     }
 
     // left 1xem for fee
